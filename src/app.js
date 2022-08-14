0c9527a539eaa200f1b66211647050843a96c41e
thanhtung <trantungg910@gmail.com>
2022-08-14 14:04:34 +0700

    fix route

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3009;
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use('/api', jsonServer.defaults(), jsonServer.router('db.json'));
+app.use('/api', jsonServer.defaults(), jsonServer.router('src/db.json'));
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
